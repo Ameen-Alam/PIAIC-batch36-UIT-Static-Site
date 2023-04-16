9b69691244479ef112846c68d7d67b5b474b88d1
Ameen Alam <ameenalam202@gmail.com>
2023-04-16 14:21:26 +0500

    add next project

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <h1>PIAIC CNC UIT</h1>
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.js</code>
